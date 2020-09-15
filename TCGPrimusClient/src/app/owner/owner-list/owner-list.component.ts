ef91d6108028b4e7128c90e25b2cd8858f60b8a6
Kevin <Kevin>
2020-09-15 22:54:46 +0700

    Error Handling

--- TCGPrimusClient/src/app/owner/owner-list/owner-list.component.ts
+++ TCGPrimusClient/src/app/owner/owner-list/owner-list.component.ts
@@ -2,26 +2,35 @@ import { Component, OnInit } from '@angular/core';
 import { RepositoryService } from './../../shared/services/repository.service';
 import { Owner } from './../../_interfaces/owner.model';
 
+import { ErrorHandlerService } from './../../shared/services/error-handler.service';
+
 @Component({
   selector: 'app-owner-list',
   templateUrl: './owner-list.component.html',
-  styleUrls: ['./owner-list.component.css']
+  styleUrls: ['./owner-list.component.css'],
 })
 export class OwnerListComponent implements OnInit {
   public owners: Owner[];
 
-  constructor(private repository: RepositoryService) { }
+  public errorMessage: string = '';
+  constructor(
+    private repository: RepositoryService,
+    private errorHandler: ErrorHandlerService
+  ) {}
 
   ngOnInit(): void {
     this.getAllOwners();
   }
 
   public getAllOwners = () => {
-    let apiAddress: string = "api/owner";
+    let apiAddress: string = 'api/owner';
     alert(apiAddress);
-    this.repository.getData(apiAddress)
-      .subscribe(res => {
-        this.owners = res as Owner[];
-      })
-  }
+    this.repository.getData(apiAddress).subscribe((res) => {
+      this.owners = res as Owner[];
+    },
+      (error) => {
+        this.errorHandler.handleError(error);
+        this.errorMessage = this.errorHandler.errorMessage;
+      });
+  };
 }
